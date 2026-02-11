007498165732a1d4b4e6a696b4c5bc199ba29254
Geras1m228 <161383804+Geras1m228@users.noreply.github.com>
2026-02-11 22:03:36 +0300

    Update 05_daily_revenue_cumulative.sql

--- sql_samples/05_daily_revenue_cumulative.sql
+++ sql_samples/05_daily_revenue_cumulative.sql
@@ -2,3 +2,18 @@
 -- Решение использует: UNNEST(product_ids), join с products, оконная сумма, LAG().
 -- Результат: date, revenue, total_revenue, revenue_change_pct.
 -- Примечания: отменённые заказы исключаются из расчёта.
+
+WITH daily_revenue AS
+(SELECT date, SUM(price) AS revenue
+ FROM (SELECT date, t1.product_id, price 
+       FROM (SELECT order_id, creation_time::DATE AS date, UNNEST(product_ids) AS product_id 
+             FROM orders
+             WHERE order_id NOT IN (SELECT order_id FROM user_actions WHERE action = 'cancel_order')) as t1
+       LEFT JOIN products AS p ON t1.product_id = p.product_id) t2
+GROUP BY 1)
+
+SELECT date, 
+       revenue, 
+       SUM(revenue) OVER(ORDER BY date) AS total_revenue, 
+       ROUND(((revenue - LAG(revenue) OVER()::numeric) / LAG(revenue) OVER() * 100), 2) AS revenue_change
+FROM daily_revenue
