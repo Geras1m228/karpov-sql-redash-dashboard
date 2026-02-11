d24a4a9f516d6e02a3710c5005d54694f08561d3
Geras1m228 <161383804+Geras1m228@users.noreply.github.com>
2026-02-11 22:01:11 +0300

    Update 04_cancel_rate_by_hour.sql

--- sql_samples/04_cancel_rate_by_hour.sql
+++ sql_samples/04_cancel_rate_by_hour.sql
@@ -2,3 +2,24 @@
 -- Решение использует: выделение часа из времени заказа, подмножества по статусам, расчёт отношения.
 -- Результат: hour, successful_orders, canceled_orders, cancel_rate.
 -- Примечания: cancel_rate = canceled / (successful + canceled).
+
+WITH orders_on_hour AS 
+(SELECT order_id, DATE_PART('hour', creation_time)::INT AS hour FROM orders),
+
+count_diliver_orders AS
+(SELECT hour, COUNT(DISTINCT order_id) AS count_deliver
+FROM orders_on_hour
+WHERE order_id IN (SELECT order_id FROM courier_actions WHERE action = 'deliver_order')
+GROUP BY 1),
+
+count_cancel_orders AS
+(SELECT hour, COUNT(DISTINCT order_id) AS count_cancel
+FROM orders_on_hour
+WHERE order_id IN (SELECT order_id FROM user_actions WHERE action = 'cancel_order')
+GROUP BY 1)
+
+SELECT d.hour, count_deliver AS successful_orders, 
+       count_cancel AS canceled_orders, 
+       ROUND((count_cancel::numeric / (count_deliver + count_cancel)), 3) AS cancel_rate
+FROM count_diliver_orders AS d LEFT JOIN count_cancel_orders AS c ON d.hour = c.hour
+ORDER BY 1;
